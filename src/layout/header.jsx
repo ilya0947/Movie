bfaa3dced07184d540428e2a24fa89589f16f550
Илья <m.i.v.89@mail.ru>
2021-12-26 22:47:38 +0500

    error

--- src/layout/header.jsx
+++ src/layout/header.jsx
@@ -1,7 +1,7 @@
 export default function Header() {
     return <nav className="green accent-3">
                 <div className="nav-wrapper">
-                    <a href="#" className="brand-logo">React Movies</a>
+                    <a href="!#" className="brand-logo">React Movies</a>
                     <ul id="nav-mobile" className="right hide-on-med-and-down">
                         <li><a href="!#">Repo</a></li>
                     </ul>
